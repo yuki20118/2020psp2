6b55af01ef4f0c547b6358130b118e30d2c35377
fujita yuki <bb35319042@ms.nagasaki-u.ac.jp>
2020-07-01 20:11:57 +0900

    0701

--- k01/k01.c
+++ k01/k01.c
@@ -8,7 +8,7 @@ extern double var_online(double val,double ave,double square_ave,int n);
 
 int main(void)
 {
-    double val,ave,square_ave,u2,ave_all,var_all,ave_n,var_n;
+    double val,ave,square_ave,u2,ave_n,var_n,ave_n1,square_ave_n1,gap;
     char fname[FILENAME_MAX];
     char buf[256];
     FILE* fp;
@@ -25,11 +25,16 @@ int main(void)
         exit(EXIT_FAILURE);
     }
 
+    ave_n1 =0;
+    square_ave_n1=0;
     while(fgets(buf,sizeof(buf),fp) != NULL){
         sscanf(buf,"%lf",&val);
-        ave_n=ave_online(val,ave,n);
-        var_n=var_online(val,ave,square_ave,n);
+        ave_n=ave_online(val,ave_n1,n);
+        square_ave=ave_online(pow(val,2),square_ave_n1,n);
+        var_n=var_online(val,ave_n1,square_ave,n);
         n++;
+        ave_n1 = ave_n;
+        square_ave_n1=square_ave;
     }
 
     if(fclose(fp) == EOF){
@@ -37,14 +42,14 @@ int main(void)
         exit(EXIT_FAILURE);
     }
 
-u2 = n*val/(n-1);
-ave_all = ave_n + sqrt(u2/n) ;
-var_all = u2 ;
+u2 = n*var_n/(n-1);
+gap = sqrt(u2/n) ;
 
-printf("ave = %lf/n ",ave_n);
-printf("var = %lf/n",var_n);
-printf("ave_all = %lf/n",ave_all);
-printf("var_all = %lf/n",var_all);
+
+printf("ave = %.2lf/n ",ave_n);
+printf("var = %.2lf/n",var_n);
+printf("ave_all = %.2lf±%.2lf/n",ave_n,gap);
+printf("var_all = %.2lf/n",u2);
 
     return 0;
 
@@ -53,17 +58,19 @@ printf("var_all = %lf/n",var_all);
 
 
 
-double ave_online(val,ave,n)
+double ave_online(double val,double ave,int n)
 {
+    double ave_n;
    ave_n = ((n-1)*ave)/n + (val/n);
    
    return ave_n;
 }
 
 
-double var_online(val,ave,square_ave,n)
+double var_online(double val,double ave,double square_ave,int n)
 {
-    var_n = (((n-1)*sqrt(ave))/n + sqrt(val)/n) - (sqrt((n-1)*ave/n + val/n));
-    
+    double var_n;
+    var_n=(((n-1)*pow(ave,2))/n + pow(val,2)/n) - pow(((n-1)*ave/n)+(val/n),2);
+
     return var_n;
 }
