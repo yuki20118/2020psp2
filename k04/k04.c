d0e71f46c24ad13ac956e0fac93c58c2247ee864
fujita yuki <bb35319042@ms.nagasaki-u.ac.jp>
2020-07-17 14:48:32 +0900

    0717

--- k04/k04.c
+++ k04/k04.c
@@ -8,32 +8,50 @@ int main(void)
 {
     FILE*fp;
     char fname[FILENAME_MAX];
-    char buf[256];
+    char buf[256],gender_mf,male,female;
     int ID,gender;
     double height;
 
-    printif("input the filename of sample:");
+    printf("input the filename of sample:");
     fgets(fname,sizeof(fname),stdin);
     fname[strlen(fname)-1]='\0';
-    printf("the filename of sample: %s\n",fname);
+
+
+    fp = fopen(fname,"r");
+
+    if(fgets(buf,sizeof(buf),fp)!=NULL){
+        sscanf(buf,"%lf",&height);
+    }
     
-    printif("input the filename of sample ID:");
+    else{
+        fputs("File open error\n",stderr);
+        exit(EXIT_FAILURE);
+    }
+    
+    printf("input the filename of sample ID:");
     fgets(fname,sizeof(fname),stdin);
     fname[strlen(fname)-1]='\0';
-    printf("the filename of sample ID: %s\n",fname);
     
     fp = fopen(fname,"r");
     if(fp==NULL){
         fputs("File open error\n",stderr);
         exit(EXIT_FAILURE);
     }
+    else{
+        sscanf(buf,"%d",&gender);
+        if(gender==1){
+            gender_mf = male ;
+        }
+        else{
+            gender_mf = female ;
+        }
+    }
 
-    printif("Which ID's data do you want? :");
+    printf("Which ID's data do you want? :");
     fgets(fname,sizeof(fname),stdin);
     fname[strlen(fname)-1]='\0';
-    printf("Which ID's data do you want? : %s\n",fname);
-    
-    printf("---");
+
+    printf("---\n");
 
     fp = fopen(fname,"r");
     if(fp==NULL){
@@ -41,18 +59,16 @@ int main(void)
         exit(EXIT_FAILURE);
     }
 
-
-    if(fgets(buf,sizeof(buf),fp)!=NULL){
+    else{
         sscanf(buf,"%d",&ID);
-
-        
     }
-
     
 
+    printf("ID : %d\n",ID);
 
+    printf("gender : %c",gender_mf);
 
-
+    printf("heights : %lf",height);
 
 
 }
