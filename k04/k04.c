5d31dc00e1b1714d720b9b94c2f5e3dbbc175265
fujita yuki <bb35319042@ms.nagasaki-u.ac.jp>
2020-07-18 00:49:17 +0900

    0718

--- k04/k04.c
+++ k04/k04.c
@@ -3,55 +3,71 @@
 #include <string.h>
 #include <math.h>
 
+struct student_data
+{
+    int gender;
+    double height;
+    int ID;
+};
+
+extern char gender_data(int gender,int i);
 
 int main(void)
 {
     FILE*fp;
     char fname[FILENAME_MAX];
-    char buf[256],gender_mf,male,female;
-    int ID,gender;
+    char buf[256],gender_mf;
+    int ID,gender,i=1;
     double height;
 
-    printf("input the filename of sample:");
-    fgets(fname,sizeof(fname),stdin);
-    fname[strlen(fname)-1]='\0';
+    struct student_data detal;
 
+
+    printf("input the filename of sample:");
+    while(fgets(buf,sizeof(buf),fp)!=NULL){
+        sscanf(buf,"%d,%lf",&gender,&height);
+        gender_mf = gender_data(gender,i);
+    }
 
     fp = fopen(fname,"r");
-
-    if(fgets(buf,sizeof(buf),fp)!=NULL){
-        sscanf(buf,"%lf",&height);
-    }
-    
-    else{
+    if(fp==NULL){
         fputs("File open error\n",stderr);
         exit(EXIT_FAILURE);
     }
-    
+
+
     printf("input the filename of sample ID:");
     fgets(fname,sizeof(fname),stdin);
     fname[strlen(fname)-1]='\0';
+
+    printf("---\n");
+
+    while(fgets(buf,sizeof(buf),fp)!=NULL){
+        sscanf(buf,"%d",&ID);
+        if(student_data[i].ID==ID){
+            printf("ID : %d\n",ID);
+            printf("gender : %c\n",gender_mf);
+            printf("height : %lf\n",height);
+        }
+        else{
+            printf("No data \n");
+        }
+
+        i++;
+    }
     
     fp = fopen(fname,"r");
     if(fp==NULL){
         fputs("File open error\n",stderr);
         exit(EXIT_FAILURE);
     }
-    else{
-        sscanf(buf,"%d",&gender);
-        if(gender==1){
-            gender_mf = male ;
-        }
-        else{
-            gender_mf = female ;
-        }
-    }
+   
+
 
     printf("Which ID's data do you want? :");
     fgets(fname,sizeof(fname),stdin);
     fname[strlen(fname)-1]='\0';
 
-    printf("---\n");
 
     fp = fopen(fname,"r");
     if(fp==NULL){
@@ -62,13 +78,17 @@ int main(void)
     else{
         sscanf(buf,"%d",&ID);
     }
-    
 
-    printf("ID : %d\n",ID);
-
-    printf("gender : %c",gender_mf);
-
-    printf("heights : %lf",height);
+}
 
 
+
+char gender_data(gender,i)
+{
+    if(student_data[i].gender==1){
+        return "male";
+    }
+    else if(student_data[i].gender==2){
+        return "female";
+    }
 }
